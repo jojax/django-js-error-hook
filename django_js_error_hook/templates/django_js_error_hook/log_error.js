00713e7065c398516f737ee6dd22df009af641dc
Ionel Cristian Mărieș <contact@ionelmc.ro>
2013-11-23 17:28:51 +0200

    Remove json encoding.

--- django_js_error_hook/templates/django_js_error_hook/log_error.js
+++ django_js_error_hook/templates/django_js_error_hook/log_error.js
@@ -4,8 +4,7 @@ function logError(details) {
 		type: "POST",
 		url: "{% url 'js-error-handler' %}",
 		data: {context: navigator.userAgent, details: details},
-		contentType: 'application/json; charset=utf-8'
-	}); 
+	});
 }
 
 window.onerror = function(error_msg, url, line_number) {
