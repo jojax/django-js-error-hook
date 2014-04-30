773500cf982dcc264df2e44cbe8e0cf64cf3cf9e
Ivan Metzlar <metzlar@gmail.com>
2014-04-30 15:07:06 -0700

    javascript indentation and global(window) namespacing

--- django_js_error_hook/templates/django_js_error_hook/utils.js
+++ django_js_error_hook/templates/django_js_error_hook/utils.js
@@ -1,80 +1,86 @@
 {% load url from future %}
-(function(){
+(function(djErrorHook, undefined){
+    
+    window.djErrorHook = djErrorHook;
 
-	function getCookie(name){
-	    var nameEQ = name + "=";
-	    var c, ca = document.cookie.split(';');
-	    
-	    for(var i = 0; i < ca.length; i++){
-		c = ca[i];
-		while(c.charAt(0) == ' ') 
-		    c = c.substring(1, c.length);
-		if(c.indexOf(nameEQ) == 0) 
-		    return c.substring(
-			nameEQ.length, c.length);
-	    }
-	    return null;
+    function getCookie(name){
+	var nameEQ = name + "=";
+	var c, ca = document.cookie.split(';');
+	
+	for(var i = 0; i < ca.length; i++){
+	    c = ca[i];
+	    while(c.charAt(0) == ' ') 
+		c = c.substring(1, c.length);
+	    if(c.indexOf(nameEQ) == 0) 
+		return c.substring(
+		    nameEQ.length, c.length);
 	}
+	return null;
+    }
 
-	function logError(details){
-	    var cookie = getCookie('csrftoken'),
-    	        query = [],
-	        data = {
-		    context: navigator.userAgent,
-		    details: details
-		},
-                xhr;
+    djErrorHook.logError = function(details){
+	var cookie = getCookie('csrftoken'),
+    	query = [],
+	data = {
+	    context: navigator.userAgent,
+	    details: details
+	},
+        xhr;
 
+	try{
+	    xhr = new ActiveXObject('Msxml2.XMLHTTP');
+	}
+	catch(e1){
 	    try{
-		xhr = new ActiveXObject('Msxml2.XMLHTTP');
+		xhr = new ActiveXObject('Microsoft.XMLHTTP');
+	    }catch(e2){
+		xhr = new XMLHttpRequest();
 	    }
-	    catch(e1){
-		try{
-		    xhr = new ActiveXObject('Microsoft.XMLHTTP');
-		}catch(e2){
-		    xhr = new XMLHttpRequest();
-		}
-	    }
-
-	    xhr.open(
-		"POST", 
-		"{% url 'js-error-handler' %}", 
-		false
-	    );
-
-	    xhr.setRequestHeader(
-		'Content-type', 
-		'application/x-www-form-urlencoded'
-	    );
-
-	    if(cookie){
-		xhr.setRequestHeader("X-CSRFToken", cookie);
-	    }
-
-	    for(var key in data){
-		query.push(
-		    encodeURIComponent(key) + 
-			'=' + 
-			encodeURIComponent(data[key])
-		);
-	    }
-	    xhr.send(query.join('&'));
 	}
 
-	window.onerror = function(error_msg, url, line_number){
-	    logError(url + ':' + line_number + ': ' + error_msg);
-	};
+	xhr.open(
+	    "POST", 
+	    "{% url 'js-error-handler' %}", 
+	    false
+	);
+
+	xhr.setRequestHeader(
+	    'Content-type', 
+	    'application/x-www-form-urlencoded'
+	);
+
+	if(cookie){
+	    xhr.setRequestHeader("X-CSRFToken", cookie);
+	}
+
+	for(var key in data){
+	    query.push(
+		encodeURIComponent(key) + 
+		    '=' + 
+		    encodeURIComponent(data[key])
+	    );
+	}
+	xhr.send(query.join('&'));
+    };
+
+    window.onerror = function(error_msg, url, line_number){
+	djErrorHook.logError(
+	    url + ':' + line_number + ': ' + error_msg);
+    };
 
     {% if proxy_console %}
     
-        var proxied_log = window.console.log;
+    var proxied_log = window.console.log;
 
-        window.console.log = function(){
-	    logError(Array.prototype.slice.call(
+    window.console.log = function(){
+	djErrorHook.logError(
+	    'CONSOLE: ' + Array.prototype.slice.call(
 		arguments).join(','));
-	    return proxied_log.apply(this, arguments);
-        };
+	return proxied_log.apply(this, arguments);
+    };
 
     {% endif %}
     
-})();
+})(
+    window.djErrorHook || {}
+);
