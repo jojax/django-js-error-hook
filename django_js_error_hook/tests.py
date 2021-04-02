c8b3bc4e7902191f68a67aa40c4b3ceb8cc6f920
Richard Barran <richard@arbee-design.co.uk>
2021-04-02 23:14:03 +0200

    Load the javascript as a static file.

--- django_js_error_hook/tests.py
+++ django_js_error_hook/tests.py
@@ -2,15 +2,13 @@ import unittest
 from django.test import TestCase
 from django.urls import reverse
 
+
 class JSErrorHookTestCase(TestCase):
     """Test project views."""
 
     @unittest.skip('Noticed test fails as part of the github-actions PR - this needs fixing in a separate ticket.')
     def test_error_handler_view(self):
         """A POST should log the error"""
-        response = self.client.post(reverse('js-error-handler'), {"details": "Description of the error by the browser javascript engine."})
-        self.assertEqual(response.status_code, 200)
-
-    def test_error_js_utils_view(self):
-        response = self.client.get(reverse('js-error-handler-js'))
+        response = self.client.post(reverse('js-error-handler'),
+                                    {"details": "Description of the error by the browser javascript engine."})
         self.assertEqual(response.status_code, 200)
