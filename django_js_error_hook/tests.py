57d12d13e740803b34579d295c38a9a7ab7711a7
Johannes Wilm <mail@johanneswilm.org>
2022-10-17 14:40:24 +0200

    reenable test

--- django_js_error_hook/tests.py
+++ django_js_error_hook/tests.py
@@ -7,9 +7,6 @@ from django.urls import reverse
 class JSErrorHookTestCase(TestCase):
     """Test project views."""
 
-    @unittest.skip(
-        "Noticed test fails as part of the github-actions PR - this needs fixing in a separate ticket.",
-    )
     def test_error_handler_view(self):
         """A POST should log the error"""
         response = self.client.post(
