4b56ff56bf94903e1c55abc6778a5a1ce86d9927
Rémy HUBSCHER <hubscher.remy@gmail.com>
2022-10-17 16:11:00 +0200

    Run black.

--- django_js_error_hook/tests.py
+++ django_js_error_hook/tests.py
@@ -6,9 +6,13 @@ from django.urls import reverse
 class JSErrorHookTestCase(TestCase):
     """Test project views."""
 
-    @unittest.skip('Noticed test fails as part of the github-actions PR - this needs fixing in a separate ticket.')
+    @unittest.skip(
+        "Noticed test fails as part of the github-actions PR - this needs fixing in a separate ticket."
+    )
     def test_error_handler_view(self):
         """A POST should log the error"""
-        response = self.client.post(reverse('js-error-handler'),
-                                    {"details": "Description of the error by the browser javascript engine."})
+        response = self.client.post(
+            reverse("js-error-handler"),
+            {"details": "Description of the error by the browser javascript engine."},
+        )
         self.assertEqual(response.status_code, 200)
--- django_js_error_hook/views.py
+++ django_js_error_hook/views.py
@@ -4,10 +4,12 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import View
 import logging
 
-ERROR_ID = getattr(settings, 'JAVASCRIPT_ERROR_ID', 'javascript_error')
-CSRF_EXEMPT = getattr(settings, 'JAVASCRIPT_ERROR_CSRF_EXEMPT', False)
-BLACKLIST_USERAGENT = getattr(settings, 'JAVASCRIPT_ERROR_USERAGENT_BLACKLIST', ['googlebot', 'bingbot'])
-BLACKLIST_ERRORS = getattr(settings, 'JAVASCRIPT_ERROR_BLACKLIST', [])
+ERROR_ID = getattr(settings, "JAVASCRIPT_ERROR_ID", "javascript_error")
+CSRF_EXEMPT = getattr(settings, "JAVASCRIPT_ERROR_CSRF_EXEMPT", False)
+BLACKLIST_USERAGENT = getattr(
+    settings, "JAVASCRIPT_ERROR_USERAGENT_BLACKLIST", ["googlebot", "bingbot"]
+)
+BLACKLIST_ERRORS = getattr(settings, "JAVASCRIPT_ERROR_BLACKLIST", [])
 
 logger = logging.getLogger(ERROR_ID)
 
@@ -18,25 +20,27 @@ class JSErrorHandlerView(View):
     def post(self, request):
         """Read POST data and log it as an JS error"""
         error_dict = request.POST.dict()
-        if hasattr(request, 'user'):
-            error_dict['user'] = request.user if request.user.is_authenticated else "<UNAUTHENTICATED>"
+        if hasattr(request, "user"):
+            error_dict["user"] = (
+                request.user if request.user.is_authenticated else "<UNAUTHENTICATED>"
+            )
         else:
-            error_dict['user'] = "<UNAUTHENTICATED>"
+            error_dict["user"] = "<UNAUTHENTICATED>"
 
         level = logging.ERROR
-        if any(useragent in error_dict['context'].lower() for useragent in BLACKLIST_USERAGENT) or \
-                any(error in error_dict['details'].lower() for error in BLACKLIST_ERRORS):
+        if any(
+            useragent in error_dict["context"].lower()
+            for useragent in BLACKLIST_USERAGENT
+        ) or any(error in error_dict["details"].lower() for error in BLACKLIST_ERRORS):
             level = logging.WARNING
 
         logger.log(
             level,
-            "Got error: \n%s", '\n'.join("\t%s: %s" % (key, value) for key, value in error_dict.items()),
-            extra={
-                'status_code': 500,
-                'request': request
-            }
+            "Got error: \n%s",
+            "\n".join("\t%s: %s" % (key, value) for key, value in error_dict.items()),
+            extra={"status_code": 500, "request": request},
         )
-        return HttpResponse('Error logged')
+        return HttpResponse("Error logged")
 
 
 if CSRF_EXEMPT:
